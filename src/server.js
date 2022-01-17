736245ba53687023391e94e21dae35fcf22b2022
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-17 22:19:49 +0900

    #0.4 Recap

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 import express from "express";
 
 const app = express();
+/* `https://expressjs.com/ko/guide/using-template-engines.html` Express와 함께 템플리트 엔진을 사용 */
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 
-console.log("Hello");
+/* `https://expressjs.com/ko/starter/static-files.html` Express에서 정적 파일(css,js) 제공 */
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+app.listen(3000, handleListen);
