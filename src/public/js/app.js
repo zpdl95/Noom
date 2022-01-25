e776bb48caaef4f783e00e96a5fd2b4e493120d2
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-25 17:04:05 +0900

    #3.2 Camera Switch

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,7 +5,6 @@ const myFace = document.getElementById("myface");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
-const deviceList = document.getElementById("deviceList");
 
 let myStream;
 let muted = false;
@@ -13,17 +12,17 @@ let cameraOff = false;
 
 async function getCameras() {
   try {
+    /* enumerateDevices() 시스템에서 사용 가능한 미디어 입출력 장치의 정보를 담은 배열을 가져옵니다 */
     const devices = await navigator.mediaDevices.enumerateDevices();
-    devices.forEach((device) => {
-      const li = document.createElement("li");
-      li.innerText = device.label;
-      deviceList.append(li);
-    });
     const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
       camerasSelect.append(option);
     });
   } catch (error) {
@@ -31,14 +30,28 @@ async function getCameras() {
   }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstrains = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: false,
-    });
+    /* getUserMedia()
+    사용자에게 권한을 요청한 후,
+      시스템의 카메라와 오디오 각각 혹은 모두 활성화하여,
+      장치의 입력 데이터를 비디오/오디오 트랙으로 포함한
+      MediaStream (en-US)을 반환 */
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstrains : initialConstrains
+    );
     myFace.srcObject = myStream;
-    await getCameras();
+    if (!deviceId) {
+      await getCameras();
+    }
   } catch (error) {
     console.dir(error);
   }
@@ -72,5 +85,10 @@ function handleCameraClick() {
   }
 }
 
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
